4fc9f11cc8e7df2e7d8474091ddf7e0cc317c7a8
zero-id <abdullatiffebriliana15@gmail.com>
2023-03-10 15:17:45 +0700

    tugas 8 js

---
+++ tugas8.js
@@ -0,0 +1,13 @@
+function Data() {
+    var data = {
+        Nama: "Zero",
+        Umur: "22 Tahun",
+        Asal: "Bandung",
+        Gender: "Laki-Laki"
+    }
+    console.log(data);
+    data.Asal = "Kuningan";
+    console.log(data);
+}
+
+Data();
